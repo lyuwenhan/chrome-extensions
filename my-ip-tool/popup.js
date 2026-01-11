1fbd7884fa32a514c7fe8f761f958ac377094174
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2026-01-10 21:53:05 -0500

    Added support with ipv6

--- my-ip-tool/popup.js
+++ my-ip-tool/popup.js
@@ -11,7 +11,12 @@ async function loadIP() {
 		const res = await fetch("https://my-ip.lyuwenhan.workers.dev/", {
 			cache: "no-store"
 		});
-		const ip = (await res.text()).trim();
+		let ip = (await res.text()).trim();
+		if (ip.length > 16) {
+			const mid = Math.ceil(ip.length / 2);
+			ip = ip.slice(0, mid) + "\n" + ip.slice(mid)
+		}
+		console.log(ip);
 		clearTimeout(loadingTimer);
 		ipEl.textContent = ip
 	} catch (e) {
